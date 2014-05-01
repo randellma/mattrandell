ff91cdd6367d4f46de06445aff1baef457de20ab
Grog140 <randellma@gmail.com>
2014-05-01 00:34:27 -0400

    Content for projects page

--- projects.php
+++ projects.php
@@ -3,14 +3,60 @@
 		<?include('overview.php');?>
 	</div>
 	<div id="pagecontent">
+	
 		<h2>Personal Projects</h2>
-		<h3 id="readsy">Readsy</h3>
-		<img src="img/readsy.png" />
+		
+		<h3 id="readsy"><a href="#readsy">Readsy</a></h3>
+		<a href="https://github.com/randellma/Readsy"><span class='symbol sidelink'>circlegithubalt</span></a>
+
+		<p>Readsy is an extension for chrome that brings embedded speed reading to any website with just a double click of the text you want to read. </p>
 		<img src="img/readsy_live_crop.png" />
-		<p>content text</p>
+		<p>I was inspired by <a href="http://www.spritzinc.com/">Spritz</a> before developers could sign up. I looked to the Chrome store for existing options but didn't like the over-complexity and need to manually select the text before speed reading then having the interface appear in a separate window of the application. The process ruined the flow of absorbing content.</p>
+		
+		<h4>Technologies</h4>
+		<ul>
+			<li>Javascript</li>
+			<li>HTML5</li>
+			<li>CSS3</li>
+			<li>Chrome Extension API</li>
+		</ul>
+		
 		<h3 id="hashtracker">Hash Tracker</h3>
-		<p>content text</p>
+		<a href="https://github.com/randellma/timetracker"><span class='symbol sidelink'>circlegithubalt</span></a>
+		
+		
+		
+		<p>Hashtracker is a web application to track your time and automatically categorize it with hashtags and other special tagging symbols</p>
+		
+		<img src="img/hashtracker_interface.png" />
+		
+		<p></p>
+		
+		<h4>Technologies</h4>
+		<ul>
+			<li>Javascript</li>
+			<li>HTML5</li>
+			<li>CSS3</li>
+			<li>Chrome Extension API</li>
+		</ul>
+		
 		<h3 id="giftology">Giftology</h3>
-		<p>content text</p>
+		<a href="https://github.com/randellma/giftology"><span class='symbol sidelink'>circlegithubalt</span></a>
+		
+		<p>Giftology is a web application to create gift exchange (secret santa) groups with exchange restrictions</p>
+		
+		<img src="img/giftology.png" />
+		
+		<p></p>
+		
+		
+		<h4>Technologies</h4>
+		<ul>
+			<li>Javascript</li>
+			<li>HTML5</li>
+			<li>CSS3</li>
+			<li>Chrome Extension API</li>
+		</ul>
+		
 	</div>
 <?include('footer.php');?>
