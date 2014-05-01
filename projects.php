964dc3ccb1d44e35676cade1fbc3c2f605357788
Grog140 <randellma@gmail.com>
2014-04-30 23:06:52 -0400

    Fix alignment when smallest
    Also added some content to projects.

---
+++ projects.php
@@ -0,0 +1,16 @@
+<?include('header.php');?>
+	<div id="sidebar">
+		<?include('overview.php');?>
+	</div>
+	<div id="pagecontent">
+		<h2>Personal Projects</h2>
+		<h3 id="readsy">Readsy</h3>
+		<img src="img/readsy.png" />
+		<img src="img/readsy_live_crop.png" />
+		<p>content text</p>
+		<h3 id="hashtracker">Hash Tracker</h3>
+		<p>content text</p>
+		<h3 id="giftology">Giftology</h3>
+		<p>content text</p>
+	</div>
+<?include('footer.php');?>
